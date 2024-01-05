4d20a71fcb7c342f4efad22bcb7ae926525dbea5
Ugurtasci <ugurtasci1291@gmail.com>
2024-01-05 23:56:50 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,22 @@
+const poke_container = document.querySelector(".poke-container");
+const search = document.querySelector(".search");
+const searchBtn = document.querySelector(".searchBtn");
+const searchInput = document / querySelector(".searchInput");
+
+const bg_color = {
+  grass: "#8BD369",
+  fire: "#FF603F",
+  water: "#3399FF",
+  bug: "#AABB22",
+  normal: "#AAAA99",
+  flying: "#9AA8FA",
+  poison: "#B76EA4",
+  electric: "#FFD34E",
+  ground: "#E2C56A",
+  fairy: "#F1A8EC",
+  psychic: "#FF6EA4",
+  fighting: "#C56E5C",
+  rock: "#C5B679",
+  dragon: "#7766EE",
+  ice: "#66CCFF",
+};
